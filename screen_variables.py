4728c9227d3038c78d008cb32eaa389db3a90790
TadeuFaustino92 <32992126+TadeuFaustino92@users.noreply.github.com>
2022-10-27 16:40:21 -0300

    Create screen_variables.py

---
+++ screen_variables.py
@@ -0,0 +1,26 @@
+tela1 = "UNID:CEPTIRJ"; lin1 = 1; col1 = 5
+tela2 = "IKJ56700A"; lin2 = 1; col2 = 2
+tela3 = "TSO/E LOGON"; lin3 = 1; col3 = 34
+tela4 = "ICH70001I"; lin4 = 1; col4 = 2
+tela5 = "<<< Menu Principal >>>"; lin5 = 4; col5 = 28
+tela6 = "CA Endevor SCM Quick Edit 18.1.00"; lin6 = 1; col6 = 22
+tela7 = "Package Foreground Options"; lin7 = 1; col7 = 25 # Removido o "Menu", pois da problema na execução
+tela8 = "PACKAGE SELECTION LIST"; lin8 = 1; col8 = 30
+tela9 = "PACKAGE DISPLAY"; lin9 = 1; col9 = 34
+tela10 = "DISPLAY - PACKAGE ID: "; lin10 = 1; col10 = 2
+tela11 = "Element Selection List"; lin11 = 1; col11 = 31
+tela12 = "Summary of Element Levels"; lin12 = 1; col12 = 29
+tela13 = "REVIEW PACKAGE"; lin13 = 1; col13 = 33
+tela14 = "EXECUTE/SUBMIT PACKAGE"; lin14 = 1; col14 = 28
+tela15 = "SUBMIT PACKAGE"; lin15 = 1; col15 = 35
+tela16 = "PACKAGE SHIPMENT"; lin16 = 1 ; col16 = 32
+tela17 = "DESTINATION SELECTION LIST"; lin17 = 1; col17 = 27
+tela18 = "CONFIRM SHIPMENT"; lin18 = 1; col18 = 32
+tela19 = "PACKAGE SHIPMENT STATUS"; lin19 = 1; col19 = 29
+stagePRD = "Environment: CEFPRD"; linStagePRD = 8; colStagePRD = 4
+asteriscos = "***"; linAsteriscos = 4; colAsteriscos = 2
+shipmentMsg = "Shipment Job Submitted"; linShipmentMsg = 1; colShipmentMsg = 58
+insert1 = "n"; linInsert1 = 20; colInsert1 = 22
+insert2 = "y"; linInsert2 = 20; colInsert2 = 45
+insert3 = "y"; linInsert3 = 21; colInsert3 = 45
+insert4 = "1"; linInsert4 = 2; colInsert4 = 15
